c870b3b2430f0eaa3c49ab951c3d80e1d6d102ca
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-25 10:28:40 -0600

    refactor for marky mark pattern

--- markov.rb
+++ markov.rb
@@ -1,41 +1,31 @@
-# sentence = "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi."
-# words = sentence.split(" ")
-# dic = Hash.new { |hash, key| hash[key] = [] }
-# words.map.with_index {|w,i| dic[w] << words[i+1]}
-#
-# seed = "Hi"
-# str = "#{seed}"
-#
-# words.length.times do
-#   seed = dic[seed].sample
-#   str += " #{seed}" unless seed.nil?
-# end
-# p str
+class MarkyMark
+  attr_accessor :str, :dic
 
-class Marky
-  attr_accessor :str, :dic  # => nil
+  def self.and_the_funky_bunch(path)
+    str = self.load_file(path)
+    words = str.split(" ")
 
-  def markov(str)
-    @str = str                                          # => "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi."
-    words = @str.split(" ")                             # => ["Hi", "my", "name", "is", "Lev", "and", "I", "like", "salad", "and", "I", "like", "pizza", "and", "my", "favorite", "movie", "is", "Pi."]
-    @dic = Hash.new { |hash, key| hash[key] = [] }      # => {}
-    words.map.with_index {|w,i| @dic[w] << words[i+1]}  # => [["my"], ["name", "favorite"], ["is"], ["Lev", "Pi."], ["and"], ["I", "I", "my"], ["like", "like"], ["salad", "pizza"], ["and"], ["I", "I", "my"], ["like", "like"], ["salad", "pizza"], ["and"], ["I", "I", "my"], ["name", "favorite"], ["movie"], ["is"], ["Lev", "Pi."], [nil]]
+    @dic = Hash.new { |hash, key| hash[key] = [] }
+    words.map.with_index {|w,i| @dic[w] << words[i+1]}
 
-    generate_string(words)  # => 19
-    p @str                  # => "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev and I like pizza and I like pizza and I like pizza and my"
+    self.generate_string(words)
   end
 
-  def generate_string(words)
-    seed = words.first        # => "Hi"
+  def self.generate_string(words)
+    str = ""
+    seed = words.first
+    words.length.times do
+      str += " #{seed}" unless seed.nil?
+      seed = @dic[seed].sample
+    end
+    p str.downcase
+  end
 
-    words.length.times do                  # => 19
-      seed = @dic[seed].sample             # => "my", "favorite", "movie", "is", "Lev", "and", "I", "like", "pizza", "and", "I", "like", "pizza", "and", "I", "like", "pizza", "and", "my"
-      @str += " #{seed}" unless seed.nil?  # => "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev and", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev and I", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev and I like", "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is...
-    end                                    # => 19
+  def self.load_file(path)
+    Dir.chdir(File.dirname(__FILE__))
+    lyrics = File.open("good_vibrations.txt", "r")
+    good_vibrations = lyrics.read.gsub("\n\n", " / ")
   end
 end
 
-m = Marky.new
-m.markov("Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi.")  # => "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev and I like pizza and I like pizza and I like pizza and my"
-m.dic
-# >> "Hi my name is Lev and I like salad and I like pizza and my favorite movie is Pi. my favorite movie is Lev and I like pizza and I like pizza and I like pizza and my"
+MarkyMark.and_the_funky_bunch("good_vibrations.txt")
